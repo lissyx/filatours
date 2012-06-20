f0bc2558d5c99640692fee754417cd7fbf1ade33
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-06-20 22:34:28 +0200

    Generating java code

--- arrets.sh
+++ arrets.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for arret in `(for stop in stops.*.txt; do cut -d'=' -f2 < $stop | cut -d'|' -f2 | sed -e 's/ /_/g'; done;) | sort | uniq`; do
-	A=$(echo $arret | sed -e 's/_/ /g');
-	echo "\"$A\",";
+for arret in `(for stop in stops.*.txt; do cut -d'=' -f2 < $stop | cut -d'|' -f2,3 | sed -e 's/ /_/g'; done;) | sort | uniq`; do
+	V=$(echo $arret | cut -d'|' -f2 | sed -e 's/_/ /g');
+	A=$(echo $arret | cut -d'|' -f1 | sed -e 's/_/ /g');
+	echo "        this.stops.put(\"$A\", \"$V\");";
 done;
