0063a5342bb8565f542650a7258def278feb7b51
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-06-20 16:29:13 +0200

    Adding real set of cities and stops

--- BusTours/src/org/gitorious/scrapfilbleu/android/BusCities.java
+++ BusTours/src/org/gitorious/scrapfilbleu/android/BusCities.java
@@ -5,16 +5,34 @@ package org.gitorious.scrapfilbleu.android;
 import android.util.Log;
 
 public class BusCities {
-	public BusCities() {
+    public BusCities() {
 
-	}
+    }
 
-	public String[] getCities() {
-		return new String[] {
-			"Ballan-Miré",
-			"Joué-lès-Tours",
-			"Tours",
-			"Saint-Pierre-des-Corps"
-		};
-	}
+    public String[] getCities() {
+        return new String[] {
+            "Ballan-Miré",
+            "Berthenay",
+            "Chambray-lès-Tours",
+            "Chanceaux-sur-Choisille",
+            "Fondettes",
+            "Joué-lès-Tours",
+            "La Riche",
+            "La Ville-aux-Dames",
+            "Luynes",
+            "Notre-Dame-d'Oé",
+            "Parçay-Meslay",
+            "Rochecorbon",
+            "Saint-Avertin",
+            "Saint-Cyr-sur-Loire",
+            "Saint-Etienne-de-Chigny",
+            "Saint-Genouph",
+            "Saint-Pierre-des-Corps",
+            "Savonnières",
+            "Tours",
+            "Vernou-sur-Brenne",
+            "Villandry",
+            "Vouvray"
+        };
+    }
 }
