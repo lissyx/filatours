8240d8476f5780a263bf0b761c51cdc3e7d77beb
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-06-21 15:29:40 +0200

    Generting Java code with geoloc

--- arrets.sh
+++ arrets.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-for arret in `(for stop in stops.*.txt; do cut -d'=' -f2 < $stop | cut -d'|' -f2,3 | sed -e 's/ /_/g'; done;) | sort | uniq`; do
-	V=$(echo $arret | cut -d'|' -f2 | sed -e 's/_/ /g');
-	A=$(echo $arret | cut -d'|' -f1 | sed -e 's/_/ /g');
-	echo "        this.stops.put(\"$A\", \"$V\");";
+for line in $(grep ^Found stops_coords.txt | sed -e 's/ /_/g'); do
+	ville=$(echo $line | cut -d'|' -f4 | sed -e 's/_/ /g');
+	arret=$(echo $line | cut -d'|' -f3 | sed -e 's/_/ /g');
+	latlon=$(echo $line | sed -e 's/.*_Degrees:_//g' -e 's/{//g' -e 's/}//g')
+	lat=$(echo $latlon | cut -d',' -f1 | cut -d':' -f2 | sed -e 's/_/ /g');
+	lon=$(echo $latlon | cut -d',' -f2 | cut -d':' -f2 | sed -e 's/_/ /g');
+	echo "        v = new HashMap<String, String>(); v.put(\"city\", \"$ville\"); v.put(\"lat\", \"$lat\"); v.put(\"lon\", \"$lon\");";
+	echo "        this.stops.put(\"$arret\", v);";
 done;
