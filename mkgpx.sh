ca0184b167cc93cd7ac4194726404f275ee31ddb
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-06-24 15:56:28 +0200

    Exporting waypoint, not tracks or routes
    Both need an ordered list of trackpoints/routepoints, which we can't
    provide.

--- mkgpx.sh
+++ mkgpx.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
+BASENAME="filbleu_ligne"
 LINES=$(cat lines.txt | sed -e 's/ /_/g' | cut -d')' -f1 | cut -d'(' -f2 | grep ^id= | cut -d'=' -f2 | sort -n)
-
-echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="FilBleuExporter">
-<metadata>Filbleu</metadata>';
 
 for LINE in $LINES; do
 	name=$(grep "(id=$LINE)" lines.txt | sed -e "s/Line(id=$LINE): //g" -e 's/name={.*} //g' -e 's/name={.*}$//g' -e "s/name='.*'//g" -e 's/number=//g' -e 's/;//g' -e 's/ | /, /g' -e 's/ $//g')
-	echo "	<trk>"
-	echo "		<name>Ligne $name</name>"
-	echo "		<trkseg>"
+	fname=$(echo $name | sed -e 's/, /,/g' -e 's/ /_/g' -e 's/,/-/g')
+	GPX=$BASENAME.$fname.gpx
+	echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="FilBleuExporter">' > $GPX
+	echo "	<metadata>Filbleu - Ligne $name</metadata>" >> $GPX
 	for stop in $(cat stops.$LINE.txt | sed -e 's/ /_/g' | cut -d'=' -f2 | sed -e 's/^>_//g'); do
 		# line:
 		# Found_a_stop_matching_stopArea:_[StopArea|676|Voltaire|Tours|||475666,50|2267709,00|1598!0!14;1597!0!14;];_Lambert2+:_{E:475666.500000,_N:2267709.000000};_Degrees:_{E:47.397322,_N:0.689295}
@@ -19,12 +18,10 @@ for LINE in $LINES; do
 		latlon=$(echo $line | sed -e 's/.*_Degrees:_//g' -e 's/{//g' -e 's/}//g')
 		lat=$(echo $latlon | cut -d',' -f1 | cut -d':' -f2 | sed -e 's/_/ /g');
 		lon=$(echo $latlon | cut -d',' -f2 | cut -d':' -f2 | sed -e 's/_/ /g');
-		echo "		<trkpt lon=\"$lon\" lat=\"$lat\">"
-		echo "			<ele>0.0</ele>"
-		echo "			<name>$arret</name>"
-		echo "		</trkpt>"
+		echo "	<wpt lon=\"$lon\" lat=\"$lat\">" >> $GPX
+		echo "		<ele>0.0</ele>" >> $GPX
+		echo "		<name>$arret</name>" >> $GPX
+		echo "	</wpt>" >> $GPX
 	done;
-	echo "		</trkseg>"
+	echo '</gpx>' >> $GPX
 done;
-
-echo '</gpx>'
