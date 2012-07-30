ceaae401f93609bd79e97673d90bf491b043e7e2
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-07-30 16:53:16 +0200

    Update stoparea.sh with new format

--- stoparea.sh
+++ stoparea.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for arret in `(for stop in stops.*.txt; do cut -d'=' -f2 < $stop | cut -d'|' -f2,3 | sed -e 's/ /_/g'; done;) | sort | uniq`; do
+for arret in `(for stop in stops.*.txt; do cut -d'=' -f2 < $stop | cut -d'|' -f2,3 | sed -e 's/ /_/g' -e 's/_\[.*\]//g'; done;) | sort | uniq`; do
 	V=$(echo $arret | cut -d'|' -f2 | sed -e 's/_/ /g');
 	A=$(echo $arret | cut -d'|' -f1 | sed -e 's/_/ /g');
 	echo "Trying \"$V - $A\""
