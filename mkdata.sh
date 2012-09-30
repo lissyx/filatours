cbe1ab3b3e018ba6c165181ffcdfc02c2be1ff28
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-09-30 14:50:31 +0200

    Updating mkdata to be ran in data/

--- mkdata.sh
+++ mkdata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for line in $(python filbleu.py --list-lines | sed -e 's/ /_/g' | cut -d')' -f1 | cut -d'(' -f2 | grep ^id= | cut -d'=' -f2 | sort -n); do
+for line in $(python ../filbleu.py --list-lines | sed -e 's/ /_/g' | cut -d')' -f1 | cut -d'(' -f2 | grep ^id= | cut -d'=' -f2 | sort -n); do
 	echo "Line $line";
-	python filbleu.py --list-stops $line > stops.$line.txt;
+	python ../filbleu.py --list-stops $line > stops.$line.txt;
 done;
