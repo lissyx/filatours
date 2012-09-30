95816784b99ffb91de8bd55256c4f7c8b9659912
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-09-30 14:50:51 +0200

    Updating stoparea to be ran in data/

--- stoparea.sh
+++ stoparea.sh
@@ -4,5 +4,5 @@ for arret in `(for stop in stops.*.txt; do cut -d'=' -f2 < $stop | cut -d'|' -f2
 	V=$(echo $arret | cut -d'|' -f2 | sed -e 's/_/ /g');
 	A=$(echo $arret | cut -d'|' -f1 | sed -e 's/_/ /g');
 	echo "Trying \"$V - $A\""
-	python filbleu.py --get-stop-coords "$V - $A"
+	python ../filbleu.py --get-stop-coords "$V - $A"
 done;
