bbf5e7ce7650b23c38d2b9260ea154edc702d5e3
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-11-24 22:15:59 +0100

    Show popup displaying stop name

--- FilATours_HTML5/js/stops.js
+++ FilATours_HTML5/js/stops.js
@@ -4,8 +4,10 @@
 'use strict';
 
 var map;
+var select;
 var stops;
 var stopsIcon;
+var selectedStopsIcon;
 
 function showmap() {
   // Options of the map
@@ -38,7 +40,19 @@ function showmap() {
       features: [],
       visibility: true
   });
+  stops.events.on({
+    "featureselected": onFeatureSelect,
+    "featureunselected": onFeatureUnselect
+  });
   map.addLayer(stops);
+
+  select = new OpenLayers.Control.SelectFeature(stops,
+    {
+      clickout: true,
+      toggle: false,
+      multiple: false,
+      hover: false
+    });
 
   stopsIcon = OpenLayers.Util.extend({}, OpenLayers.Feature.Vector.style['default']);
   stopsIcon.graphicWidth = 21;
@@ -47,6 +61,44 @@ function showmap() {
   stopsIcon.graphicYOffset = -25;
   stopsIcon.externalGraphic = 'ext/OpenLayers/img/marker-green.png';
   stopsIcon.fillOpacity = 1;
+
+  selectedStopsIcon = OpenLayers.Util.extend({}, OpenLayers.Feature.Vector.style['default']);
+  selectedStopsIcon.graphicWidth = 21;
+  selectedStopsIcon.graphicHeight = 25;
+  selectedStopsIcon.graphicXOffset = -(21/2);
+  selectedStopsIcon.graphicYOffset = -25;
+  selectedStopsIcon.externalGraphic = 'ext/OpenLayers/img/marker-blue.png';
+  selectedStopsIcon.fillOpacity = 1
+  select.selectStyle = selectedStopsIcon;
+
+  map.addControl(select);
+  select.activate();
+}
+
+function featureToHtml(feature) {
+  var o = feature.data;
+  return '<h1>' + o._name + '</h1>';
+}
+
+function onFeatureSelect(event) {
+  var f = event.feature;
+  var popup = new OpenLayers.Popup.FramedCloud("chicken",
+    f.geometry.getBounds().getCenterLonLat(),
+    new OpenLayers.Size(200, 100),
+    featureToHtml(f),
+    null,
+    true,
+    null);
+  map.addPopup(popup, false);
+}
+
+function onFeatureUnselect(event) {
+  var f = event.feature;
+  if (f.popup) {
+    map.removePopup(f.popup);
+    f.popup.destroy();
+    delete f.popup;
+  }
 }
 
 function showstops() {
@@ -56,7 +108,11 @@ function showstops() {
     var se = all[s];
     var stopPos = new OpenLayers.LonLat(se._longitude, se._latitude)
       .transform(map.options.displayProjection, map.options.projection);
-    features.push(new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(stopPos.lon, stopPos.lat), {}, stopsIcon));
+    features.push(
+        new OpenLayers.Feature.Vector(
+          new OpenLayers.Geometry.Point(stopPos.lon, stopPos.lat),
+          se,
+          stopsIcon));
   }
   stops.addFeatures(features);
   map.zoomToExtent(stops.getDataExtent());
