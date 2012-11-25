42254810f75e6ca8e6ead0dded12f715c439a78a
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2012-11-25 13:02:48 +0100

    Re-use stops.js for whereami interface

--- FilATours_HTML5/js/whereami.js
+++ FilATours_HTML5/js/whereami.js
@@ -3,7 +3,6 @@
 
 'use strict';
 
-var map;
 var wid;
 var prev = undefined;
 
@@ -11,49 +10,15 @@ var markers;
 var curPosMarker;
 var curPosIcon;
 
-var stops;
-var stopsIcon;
-
 function whereami() {
-  // Options of the map
-  var options = {
-    projection: new OpenLayers.Projection("EPSG:900913"),
-    displayProjection: new OpenLayers.Projection("EPSG:4326"),
-    units: "m",
-    maxResolution: 156543.0339,
-    maxExtent: new OpenLayers.Bounds(-20037508.34, -20037508.34,
-                     20037508.34, 20037508.34),
-    controls: [
-      new OpenLayers.Control.Attribution(),
-      new OpenLayers.Control.TouchNavigation({
-          dragPanOptions: {
-              enableKinetic: true
-          }
-      }),
-      new OpenLayers.Control.Zoom()
-    ],
-    numZoolLevels: 10,
-  };
-
-  // Initialiaze the map and the main layer
-  map = new OpenLayers.Map('map', options);
-
-  var mapnik = new OpenLayers.Layer.OSM("OpenStreetMap (Mapnik)");
-  map.addLayers([mapnik]);
-
   markers = new OpenLayers.Layer.Markers( "Markers" );
   map.addLayer(markers);
-
-  stops = new OpenLayers.Layer.Markers( "Stops" );
-  map.addLayer(stops);
 
   var size = new OpenLayers.Size(21,25);
   var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
   curPosIcon = new OpenLayers.Icon('ext/OpenLayers/img/marker-gold.png', size, offset);
   curPosMarker = new OpenLayers.Marker(new OpenLayers.LonLat(0, 0), curPosIcon)
   markers.addMarker(curPosMarker);
-
-  stopsIcon = new OpenLayers.Icon('ext/OpenLayers/img/marker-green.png', size, offset);
 }
 
 function error(msg) {
@@ -66,23 +31,6 @@ function updateCurPosMarker(newPos) {
   markers.addMarker(curPosMarker);
 }
 
-function updateNearestStops(coords, newPos) {
-  for (var m in stops.markers) {
-    var sm = stops.markers[m];
-    console.debug("removing " + m + "::" + sm);
-    stops.removeMarker(sm);
-  }
-  var closests = BusStops.getNearestStop(coords);
-  for (var s in closests) {
-    var se = closests[s];
-    var stopPos = new OpenLayers.LonLat(se._longitude, se._latitude)
-      .transform(map.options.displayProjection, map.options.projection);
-    var mm = new OpenLayers.Marker(stopPos, stopsIcon.clone());
-    stops.addMarker(mm);
-  }
-  map.zoomToExtent(stops.getDataExtent());
-}
-
 function showPosition(ev) {
   updateStatus("Updating location.");
   var olc = new OpenLayers.LonLat(
@@ -91,9 +39,8 @@ function showPosition(ev) {
       .transform(
         map.options.displayProjection,
         map.options.projection);
-  map.setCenter(olc, 14);
+  map.setCenter(olc, 15);
   updateCurPosMarker(olc);
-  updateNearestStops(ev.coords, olc);
 
   if (prev) {
     var dist = distance(prev, ev.coords);
