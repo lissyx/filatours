f6d942754f4a78b018ce43b2dc9b7c16ced92806
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2013-07-28 15:50:33 +0200

    Coding Style

--- BusTours/src/org/gitorious/scrapfilbleu/android/SeasonPicker.java
+++ BusTours/src/org/gitorious/scrapfilbleu/android/SeasonPicker.java
@@ -6,34 +6,34 @@ import java.util.Calendar;
 import android.util.Log;
 
 public class SeasonPicker {
-	public static Calendar getTramProd() {
-		Calendar prodTram = Calendar.getInstance();
-		prodTram.set(2013, 7, 30);
-		return prodTram;
-	}
+    public static Calendar getTramProd() {
+        Calendar prodTram = Calendar.getInstance();
+        prodTram.set(2013, 7, 30);
+        return prodTram;
+    }
 
-	public static boolean checkDate(String season, int year, int month, int day) {
-		Calendar date = Calendar.getInstance();
-		date.set(year, month, day);
+    public static boolean checkDate(String season, int year, int month, int day) {
+        Calendar date = Calendar.getInstance();
+        date.set(year, month, day);
 
-		return date.after(SeasonPicker.getTramProd()) && season.equals("tram");
-	}
+        return date.after(SeasonPicker.getTramProd()) && season.equals("tram");
+    }
 
-	public static String pickFromDate(Calendar date) {
-		String seasonPicked;
+    public static String pickFromDate(Calendar date) {
+        String seasonPicked;
 
-		if (date.after(SeasonPicker.getTramProd())) {
-			seasonPicked = "tram";
-		} else {
-			seasonPicked = "classic";
-		}
+        if (date.after(SeasonPicker.getTramProd())) {
+            seasonPicked = "tram";
+        } else {
+            seasonPicked = "classic";
+        }
 
-	        Log.e("BusTours:SeasonPicker", "Picked " + seasonPicked);
+            Log.e("BusTours:SeasonPicker", "Picked " + seasonPicked);
 
-		return seasonPicked;
-	}
+        return seasonPicked;
+    }
 
-	public static String pick() {
-		return Calendar.getInstance();
-	}
+    public static String pick() {
+        return Calendar.getInstance();
+    }
 }
