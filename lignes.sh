925d11272857d9fad53a3bd2c42dbf8f1ff9a805
Alexandre Lissy <alexandre.lissy@etu.univ-tours.fr>
2013-10-06 23:04:21 +0200

    Fixing lines generation

--- lignes.sh
+++ lignes.sh
@@ -9,16 +9,18 @@ for line in $(grep ^Found stops_coords.txt | sed -e 's/ /_/g'); do
 		id=$(echo $lid | sed -e 's/A//g' -e 's/B//g')
 		if [ -f stops.$id.txt ]; then
 			name=$(grep "(id=$id)" lines.txt | sed -e "s/Line(id=$id): //g" -e 's/name={.*} //g' -e 's/name={.*}$//g' -e "s/name='.*'//g" -e 's/number=//g' -e 's/;//g' -e 's/ | / /g' -e 's/ $//g')
-			echo $lid | egrep -q 'A|B'
+			echo $name | egrep -q '.+A|.+B'
 			if [ $? -eq 0 ]; then
-				# echo "lid=$lid ;; name==$name"
-				for na in $name; do
-					res=$(echo $na | sed -e "s/$lid//g")
-					if [ "$res" = "0" ]; then
-						# echo "lid=$lid ;; na==$na ==> $res"
-						finalname=$na
-					fi
-				done;
+				#echo "lid=$lid ;; name==$name"
+				finalname=$name
+				#for na in $name; do
+					#res=$(echo $na | sed -e "s/$name//g")
+					#echo "$na -- $res"
+					#if [ "$res" = "0" ]; then
+					#	echo "lid=$lid ;; na==$na ==> $res"
+					#	finalname=$na
+					#fi
+				#done;
 			else
 				finalname=$(echo $name | sed -e 's/ /_/g')
 			fi;
